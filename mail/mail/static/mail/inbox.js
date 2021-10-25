04537e24037d9fb72727f8a6917d643f6fde8c4a
whejy <john.wheldale@outlook.com>
2021-10-25 13:30:58 +1030

    added archive functionality

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -9,6 +9,30 @@ document.addEventListener('DOMContentLoaded', function() {
   // Submit-button listener
   document.querySelector('form').addEventListener('submit', () => get_data(event));
 
+  // Archive or un-archive an email
+  var archiveBTN = document.querySelector('#archiveBTN');
+  if (archiveBTN) {
+    archiveBTN.addEventListener('click', () => {
+      console.log(archiveBTN.value);
+      if (archiveBTN.classList.contains('moveArchive')) {
+        fetch(`/emails/${archiveBTN.value}`, {
+          method: 'PUT',
+          body: JSON.stringify({
+          archived: true
+          })
+        })
+      } else {
+        fetch(`/emails/${archiveBTN.value}`, {
+          method: 'PUT',
+          body: JSON.stringify({
+          archived: false
+          })
+        })
+      }
+      location.reload();
+    });
+  }
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -20,6 +44,7 @@ function compose_email() {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#email-display').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#archiveBTN').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -28,7 +53,7 @@ function compose_email() {
 
 }
 
-
+// Compose email
 function get_data(event) {
 
   event.preventDefault();
@@ -47,6 +72,7 @@ function get_data(event) {
     console.log(result);
     load_mailbox('sent');
   });
+
 }
 
 
@@ -56,6 +82,7 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#email-display').style.display = 'none';
+  document.querySelector('#archiveBTN').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -64,20 +91,18 @@ function load_mailbox(mailbox) {
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
-      // Print emails
-      console.log(emails);
-  
+
       // Display emails
       for (let i = 0; i < emails.length; i++) {
         var el = document.createElement('div');
         if (`${mailbox}` == 'sent') {
-          el.innerHTML = `<b>Recipients:</b> ${emails[i].recipients}<br><b>Subject:</b> ${emails[i].subject}<br><b>Sent:</b> ${emails[i].timestamp}`
+          el.innerHTML = `<b>Recipients:</b> ${emails[i].recipients} <br><b>Subject:</b> ${emails[i].subject}<br><b>Sent:</b> ${emails[i].timestamp}`
         }
         else {
           el.innerHTML = `<b>Sender:</b> ${emails[i].sender}<br><b>Subject:</b> ${emails[i].subject}<br><b>Received:</b> ${emails[i].timestamp}`
         }
         el.addEventListener('click', () => {
-          load_email(emails[i].id);
+          load_email(emails[i].id, mailbox);
         })
         el.classList.add("emailList");
         if (emails[i].read == true) {
@@ -89,18 +114,42 @@ function load_mailbox(mailbox) {
 
 }
 
-function load_email(id) {
+function load_email(id, mailbox) {
 
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#email-display').style.display = 'block';
+  document.querySelector('#archiveBTN').style.display = 'block';
+
+  // Archive-button behaviour. Classlist modified to determine action on click.
+  var button = document.querySelector('#archiveBTN');
+  if (mailbox == 'sent') {
+    button.style.display = "none";
+  } else {
+    button.style.display = "block";
+    button.value = `${id}`;
+    if (mailbox == 'archive') {
+      button.innerHTML = 'Move to Inbox';
+      button.classList.add('moveInbox');
+      if (button.classList.contains('moveArchive')) {
+        button.classList.remove('moveArchive');
+        console.log(button);
+      }
+    } else {
+        button.innerHTML = 'Archive Email';
+        button.classList.add('moveArchive');
+        if (button.classList.contains('moveInbox')) {
+          button.classList.remove('moveInbox');
+          console.log(button);
+      }
+    }
+  }
 
   // Get email by id
   fetch(`/emails/${id}`)
   .then(response => response.json())
   .then(email => {
-    console.log(email);
     document.getElementById('email-display').innerHTML = `
     <span class="emailDetails"> <b>Sender:</b> ${email.sender} </span>
     <span class="emailDetails"> <b>Recipients:</b> ${email.recipients}</span>
@@ -116,4 +165,5 @@ function load_email(id) {
       read: true
     })
   })
+  
 }
